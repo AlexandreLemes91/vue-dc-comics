dfc3df72d3b2371d91aaf4f260d096095201d3fc
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-05-04 22:58:33 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <h1>HELLO ALEX</h1>
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
+  Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+body{
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+
+#app{
+  display: flex;
+  justify-content: center;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+
+.container {
+  width: 1300px;
 }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,83 @@
+<template>
+    <header>
+        <div class="container">
+            <img :src="img.url" :alt="img.name">
+            <ul>
+                <li v-for="(link, index) in links" :key="link.text + index"><a href="link.  url">{{link.text}}</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+    data(){
+        return{
+            img: {name:'dc-logo', url: require('@/assets/img/dc-logo.png'),},
+
+            links: [
+                {text: 'characters', url: "/"},
+                {text: 'comics', url: "/"},
+                {text: 'movies', url: "/"},
+                {text: 'tv', url: "/"},
+                {text: 'games', url: "/"},
+                {text: 'collectibles', url: "/"},
+                {text: 'videos', url: "/"},
+                {text: 'fans', url: "/"},
+                {text: 'news', url: "/"},
+                {text: 'shop', url: "/"},
+            ],
+        }
+    },
+}
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+img{
+    max-width: 80px;
+}
+
+ul{
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+ul li{
+    padding: 40px 0;
+    margin: 0 15px 0;
+    border-top: 3px solid transparent;
+    border-bottom: 3px solid transparent;
+    transition: border-bottom-color 0.3s;
+}
+
+li a{
+    color: #494b4e;
+    text-decoration: none;
+    font-size: 14px;
+    text-transform: uppercase;
+    font-weight: 700;
+    transition: color 0.5s;
+}
+
+ul li:last-child{
+    margin-right: 0pxs;
+}
+
+ul li:hover{
+    border-bottom-color: #0282f9;
+}
+
+ul li:hover a{
+    color: #0282f9;
+}
+</style>
